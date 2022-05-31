a29858b32317cc8203ae4192483abf5a95110e75
Eliazar Agwaya <eliazar.magero@student.moringaschool.com>
2022-05-31 22:09:09 +0300

    commit tree d24f0da1ab1e60c6f16cb8e70c3d92c61acbe2f8

---
+++ index.js
@@ -0,0 +1,39 @@
+var a, b;
+function add(a, b){
+    return a+b;
+}
+console.log(add(4,2));
+function subtract(a, b){
+    return a-b;
+}
+console.log(subtract(7,5));
+
+function multiply(a, b){
+    return a*b;
+}
+console.log(multiply(3, 6));
+
+function divide(a, b){
+    return a/b;
+}
+console.log(divide(15,3));
+
+function increment(n){
+    return n+1;
+}
+console.log(increment(a));
+
+function decrement(n){
+    return n-1;
+}
+console.log(decrement(a));
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+console.log(makeInt(a));
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal(a))
